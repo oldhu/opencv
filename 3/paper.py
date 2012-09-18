848d294e62d39647781819b101e48ff7a66b692c
hu <me@oldhu.com>
2012-09-18 15:52:08 +0800

    deskew works

--- 3/paper.py
+++ 3/paper.py
@@ -14,10 +14,11 @@ def resize(img):
 
 img = cv2.imread(sys.argv[1])
 img = resize(img)
-(height, _, _) = img.shape
+(height, width, _) = img.shape
 img2 = img.copy()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# gray = cv2.equalizeHist(gray)
 cv2.imshow('gray', gray)
 cv2.moveWindow('gray', 100, 0)
 
@@ -27,18 +28,34 @@ cv2.moveWindow('threshold', 600, 0)
 
 contours, hier = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
+resultcnt = None
+hull = None
+
 for cnt in contours:
     hull = cv2.convexHull(cnt)    # find the convex hull of contour
     cv2.drawContours(img2, [hull], 0, (0, 255, 0), 1)
     hull = cv2.approxPolyDP(hull, 0.1 * cv2.arcLength(hull, True), True)
-    if cv2.contourArea(cnt) > 5000:  # remove small areas like noise etc
+    if cv2.contourArea(cnt) > 10000:  # remove small areas like noise etc
         if len(hull) == 4:
-            print hull
+            resultcnt = cnt
             cv2.drawContours(img, [hull], 0, (0, 255, 0), 2)
+            break
+
+box = np.int32(cv2.cv.BoxPoints(cv2.minAreaRect(resultcnt)))
+cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+
+src = np.float32(np.reshape(hull, (4, 2)))
+# src = np.float32(hull)
+dst = np.float32(box)
+mat = cv2.getPerspectiveTransform(src, dst)
+newimg = cv2.warpPerspective(img, mat, (width, height))
 
 cv2.imshow('contour', img2)
 cv2.moveWindow('contour', 100, height + 50)
 cv2.imshow('img', img)
 cv2.moveWindow('img', 600, height + 50)
+
+cv2.imshow('newimg', newimg)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
