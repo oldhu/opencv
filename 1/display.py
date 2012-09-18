7d715bc2c3d17b1f6a0e7c1c4ec4a7e557f23b3c
hu <me@oldhu.com>
2012-09-18 11:52:52 +0800

    use cv2

--- 1/display.py
+++ 1/display.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import cv
+import cv2
 import sys
 
 if len(sys.argv) != 2:
   print 'Usage: display.py ImageToLoadAndDisplay'
   exit(1)
 
-img = cv.LoadImage(sys.argv[1])
-cv.NamedWindow("Display window")
-cv.ShowImage("Display window", img)
-cv.WaitKey()
+img = cv2.imread(sys.argv[1])
+cv2.namedWindow("Display window")
+cv2.imshow("Display window", img)
+cv2.waitKey(0)
